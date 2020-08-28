8f47f12928245326638f24aea887863318aa2356
Owen Niblock <owenniblock@github.com>
2020-08-28 13:24:15 +0100

    feat: messing around with blocks, block heights etc

--- test/client.test.js
+++ test/client.test.js
@@ -14,7 +14,32 @@ test('creates a fill without errors', async function (done) {
 });
 
 test('clears everything without errors', async function (done) {
-        await client.fill(-64, 24, -64, 64, 24, 64, false, 'air')
+        await client.fill(-64, 20, -64, 64, 20, 64, false, 'air')
+
+        done();
+});
+
+test('creates rainbow test without errors', async function (done) {
+        await client.createRainbowMap(20, false);
+
+        done();
+});
+
+test('creates height test without errors', async function (done) {
+        await client.createHeightMap(20, false, 250);
+
+        done();
+});
+
+test('deletes height test without errors', async function (done) {
+        await client.deleteHeightMap(20, false);
+
+        done();
+});
+
+test('creates a blank page', async function (done) {
+        // We have to make it slightly wider than the map so we don't get the lightening affect from height.
+        await client.fill(-65, 19, -65, 65, 19, 65, false, 'quartz_block')
 
         done();
 });
